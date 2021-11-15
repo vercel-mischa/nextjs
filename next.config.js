f8c507a56d3324f6b60abfca77c8ca10d9e9ab2c
vercel-mischa <94338118+vercel-mischa@users.noreply.github.com>
2021-11-14 22:38:42 -0800

    add vercel redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+    async redirects() {
+    return [
+      {
+        source: '/vercel',
+        destination: 'https://vercel.com',
+        permanent: false,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
